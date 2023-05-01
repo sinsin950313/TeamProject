8e883bb07b2451792e5fc99a33c2143393082e48
sin <sinsin950313@gmail.com>
2023-05-01 18:37:40 +0900

    <Chore> : Edge 검출

--- data/shader/SSB/ScreenShader.hlsl
+++ data/shader/SSB/ScreenShader.hlsl
@@ -115,7 +115,55 @@ float4 GetAmbient()
 
 bool IsBoundary(float2 uv)
 {
-	return false;
+	/*float depth = DepthMap.Sample(Sampler, uv).x;
+	depth = depth - 0.998;
+	depth = depth / 0.002f;
+	if(0.8f < depth)
+	{
+		return false;
+	}
+
+	float4 textureColor = DepthMap.Sample(Sampler, uv);
+	int		g_iTexSizeX = 1600;
+	int		g_iTexSizeY = 900;
+	// -1 0 1
+	// -2 0 2
+	// -1 0 1
+	float dx = 1.0f/g_iTexSizeX;
+	float dy = 1.0f/g_iTexSizeY;
+
+	float4 tl = DepthMap.Sample(Sampler, float2(uv.x - dx, uv.y - dy));
+	float4 l = DepthMap.Sample(Sampler, float2(uv.x - dx, uv.y));
+	float4 bl = DepthMap.Sample(Sampler, float2(uv.x - dx, uv.y + dy));
+	float4 t = DepthMap.Sample(Sampler, float2(uv.x, uv.y - dy));
+	float4 b = DepthMap.Sample(Sampler, float2(uv.x, uv.y + dy));
+	float4 tr = DepthMap.Sample(Sampler, float2(uv.x + dx, uv.y - dy));
+	float4 r = DepthMap.Sample(Sampler, float2(uv.x + dx, uv.y));
+	float4 br = DepthMap.Sample(Sampler, float2(uv.x + dx, uv.y + dy));
+	
+	float4 SobelX = -tl - 2.0f * l - bl + tr + 2.0f * r + br;
+
+	// -1 -2 -1
+	// 0 0 0
+	// 1 2 1
+	float4 SobelY = -tl - 2.0f *t - tr + bl + 2.0f *b + br;
+
+	//float3 N = normalize(float3(-SobelX.x, -SobelY.x, 1.0f));
+	//N = N *0.5f +0.5f;
+
+	float4 SobelResult = abs(SobelX) + abs(SobelY);
+	//float deltaNormal = (SobelResult.x + SobelResult.y + SobelResult.z ) / 3;
+	float deltaNormal = SobelResult.x  / 0.002f;
+		
+	if (deltaNormal < 0.1f)
+	{
+		return false;
+	}
+	else 
+	{
+		return true;
+	}	*/
+
 	float depth = DepthMap.Sample(Sampler, uv).x;
 	depth = depth - 0.998;
 	depth = depth / 0.002f;
@@ -124,40 +172,39 @@ bool IsBoundary(float2 uv)
 		return false;
 	}
 
-	float textureColor = DepthMap.Sample(Sampler, uv).x;
+	float4 textureColor = NormalMap.Sample(Sampler, uv);
 	int		g_iTexSizeX = 1600;
 	int		g_iTexSizeY = 900;
-
 	// -1 0 1
 	// -2 0 2
 	// -1 0 1
 	float dx = 1.0f/g_iTexSizeX;
 	float dy = 1.0f/g_iTexSizeY;
 
-	float tl = DepthMap.Sample(Sampler, float2(uv.x - dx, uv.y - dy)).x;
-	float l = DepthMap.Sample(Sampler, float2(uv.x - dx, uv.y)).x;
-	float bl = DepthMap.Sample(Sampler, float2(uv.x - dx, uv.y + dy)).x;
-	float t = DepthMap.Sample(Sampler, float2(uv.x, uv.y - dy)).x;
-	float b = DepthMap.Sample(Sampler, float2(uv.x, uv.y + dy)).x;
-	float tr = DepthMap.Sample(Sampler, float2(uv.x + dx, uv.y - dy)).x;
-	float r = DepthMap.Sample(Sampler, float2(uv.x + dx, uv.y)).x;
-	float br = DepthMap.Sample(Sampler, float2(uv.x + dx, uv.y + dy)).x;
+	float4 tl = NormalMap.Sample(Sampler, float2(uv.x - dx, uv.y - dy));
+	float4 l = NormalMap.Sample(Sampler, float2(uv.x - dx, uv.y));
+	float4 bl = NormalMap.Sample(Sampler, float2(uv.x - dx, uv.y + dy));
+	float4 t = NormalMap.Sample(Sampler, float2(uv.x, uv.y - dy));
+	float4 b = NormalMap.Sample(Sampler, float2(uv.x, uv.y + dy));
+	float4 tr = NormalMap.Sample(Sampler, float2(uv.x + dx, uv.y - dy));
+	float4 r = NormalMap.Sample(Sampler, float2(uv.x + dx, uv.y));
+	float4 br = NormalMap.Sample(Sampler, float2(uv.x + dx, uv.y + dy));
 	
-	float SobelX = -tl - 2.0f * l - bl + tr + 2.0f * r + br;
+	float4 SobelX = -tl - 2.0f * l - bl + tr + 2.0f * r + br;
 
 	// -1 -2 -1
 	// 0 0 0
 	// 1 2 1
-	float SobelY = -tl - 2.0f * t - tr + bl + 2.0f * b + br;
+	float4 SobelY = -tl - 2.0f *t - tr + bl + 2.0f *b + br;
 
 	//float3 N = normalize(float3(-SobelX.x, -SobelY.x, 1.0f));
 	//N = N *0.5f +0.5f;
 
-	float SobelResult = (abs(SobelX) + abs(SobelY)) / 2.0f;
-	SobelResult = SobelResult / 0.002f;
-	SobelResult = SobelResult - 0.998f;
+	float4 SobelResult = abs(SobelX) + abs(SobelY);
+	float deltaNormal = (SobelResult.x + SobelResult.y + SobelResult.z ) / 3;
+	float deltaDepth = SobelResult.w;
 		
-	if (0.7f < SobelResult)
+	if (deltaNormal < 0.7f && deltaDepth < 0.05f)
 	{
 		return false;
 	}
